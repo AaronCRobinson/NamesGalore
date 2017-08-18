bb1278fa0a87595c43bf0feeb977f4a56617b864
Aaron C Robinson <robinson.aaron.c@qq.com>
2017-08-18 09:42:18 -0400

    Initial commit

---
+++ Source/NamesGalore/HarmonyPatches.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Verse;
+using RimWorld;
+using Harmony;
+using System.Reflection.Emit;
+
+namespace NamesGalore
+{
+    [StaticConstructorOnStartup]
+    class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.whyisthat.namesgalore.main");
+            harmony.Patch(AccessTools.Method(typeof(PawnBioAndNameGenerator), nameof(PawnBioAndNameGenerator.GeneratePawnName)), null, null, new HarmonyMethod(typeof(HarmonyPatches), nameof(ReduceSolidNameChance)));
+        }
+
+        public static IEnumerable<CodeInstruction> ReduceSolidNameChance(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach(CodeInstruction instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_R4 && (float)instruction.operand == 0.5f)
+                    yield return new CodeInstruction(OpCodes.Ldc_R4, 0.1f);
+                else
+                    yield return instruction;
+            }
+        }
+
+    }
+}
---
+++ Source/NamesGalore/NameInjector.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using Harmony;
+
+namespace NamesGalore
+{
+    [StaticConstructorOnStartup]
+    public static class NameInjector
+    {
+        static FieldInfo FI_banks = AccessTools.Field(typeof(PawnNameDatabaseShuffled), "banks");
+        // note: handle languages better...
+        static string relativeLanguagePath = Path.Combine("Languages", LanguageDatabase.activeLanguage.folderName);
+
+        static NameInjector()
+        {
+            Log.Message("NamesGalore: Injecting Names");
+            Dictionary<PawnNameCategory, NameBank> banks = (Dictionary < PawnNameCategory, NameBank > )FI_banks.GetValue(null);
+            NameBank nameBank = banks[PawnNameCategory.HumanStandard];
+            AddNamesFromFile(nameBank, PawnNameSlot.First, Gender.Male, "First_Male.txt");
+            AddNamesFromFile(nameBank, PawnNameSlot.First, Gender.Female, "First_Female.txt");
+            AddNamesFromFile(nameBank, PawnNameSlot.Nick, Gender.Male, "Nick_Male.txt");
+            AddNamesFromFile(nameBank, PawnNameSlot.Nick, Gender.Female, "Nick_Female.txt");
+            AddNamesFromFile(nameBank, PawnNameSlot.Nick, Gender.None, "Nick_Unisex.txt");
+            AddNamesFromFile(nameBank, PawnNameSlot.Last, Gender.None, "Last.txt");
+        }
+
+        private static string ComposePath(string fileName)
+        {
+            return Path.Combine(Path.Combine(NamesGaloreMod.settings.rootDir, relativeLanguagePath), Path.Combine("Names", fileName));
+        }
+
+        private static void AddNamesFromFile(NameBank nameBank, PawnNameSlot slot, Gender gender, string fileName)
+        {
+            nameBank.AddNames(slot, gender, LinesFromFile(ComposePath(fileName)));
+        }
+
+        private static IEnumerable<string> LinesFromFile(string filePath)
+        {
+            string rawText = GenFile.TextFromRawFile(filePath);
+            foreach (string line in GenText.LinesFromString(rawText))
+            {
+                yield return line;
+            }
+        }
+    }
+}
