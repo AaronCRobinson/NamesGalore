2db2353cbb134fd615e8fc07c00b6b95745bca9c
Aaron C Robinson <>
2018-06-30 13:58:00 -0400

    V1.0 Update

--- Source/copyFiles.bat
+++ Source/copyFiles.bat
@@ -4,4 +4,5 @@ SET "SolutionDir=C:\Users\robin\Desktop\Games\RimWorld Modding\Source\NamesGalor
 @echo on
 
 xcopy /S /Y "%SolutionDir%\..\About\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\About\"
+xcopy /S /Y "%SolutionDir%\..\Assemblies\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Assemblies\"
 xcopy /S /Y "%SolutionDir%\..\Languages\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Languages\"
